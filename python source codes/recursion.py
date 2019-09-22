01f47db45d413e85efc7dd2c524ae0ccb58d14bc
Kevin Cui JY <kevincui1230@gmail.com>
2019-09-21 23:08:26 -0500

    update format

--- python source codes/recursion.py
+++
@@ -1,8 +0,0 @@
-def Fibonacci(init_num):
-    if init_num == 0 or init_num == 1:
-        return 1
-    else:
-        return Fibonacci(init_num-1) + Fibonacci(init_num-2)
-
-for n in range(24):
-    print(Fibonacci(n))
